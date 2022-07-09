abc209caa029cd733bc9477eda14d1fcf0829a53
Pjijin <pjijin1@gmail.com>
2022-07-09 19:29:41 +0530

    📦 New: prisma seed db

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # w3auth
+
+# Plan
+
+-   generate jwt, nounce
+-   set user field, content on the jwt token
+-   nft gated access
+-   token gated access
+-   allowlist verify login
---
+++ db/seed.ts
@@ -0,0 +1,114 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.settings.upsert({
+        where: { name: 'access_allowlist_only' },
+        update: {},
+        create: {
+            name: 'access_allowlist_only',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'nft_access_only' },
+        update: {},
+        create: {
+            name: 'nft_access_only',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'token_gated_access_only' },
+        update: {},
+        create: {
+            name: 'token_gated_access_only',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'custom_jwt_claim' },
+        update: {},
+        create: {
+            name: 'custom_jwt_claim',
+            value: '',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_eth' },
+        update: {},
+        create: {
+            name: 'enable_eth',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_bnb' },
+        update: {},
+        create: {
+            name: 'enable_bnb',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_matic' },
+        update: {},
+        create: {
+            name: 'enable_matic',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_glmr' },
+        update: {},
+        create: {
+            name: 'enable_glmr',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_ftm' },
+        update: {},
+        create: {
+            name: 'enable_ftm',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_movr' },
+        update: {},
+        create: {
+            name: 'enable_movr',
+            value: 'false',
+        },
+    })
+
+    await prisma.settings.upsert({
+        where: { name: 'enable_avax' },
+        update: {},
+        create: {
+            name: 'enable_avax',
+            value: 'false',
+        },
+    })
+
+
+}
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
