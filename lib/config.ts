38e6e8ecd641b1f67c074a3cef42fc8b2b99abf7
Pjijin <pjijin1@gmail.com>
2022-07-27 22:48:51 +0530

    📦 New: link wallet api

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 const Config = {
     ItemsPerPage: 10,
-    SignMessageText: `Welcome to w3auth, please sign this message to verify your identity. Nonce:`
+    SignMessageText: `Welcome to w3auth, please sign this message to verify your identity.`,
 }
 
 export default Config;
--- lib/lang.ts
+++ lib/lang.ts
@@ -2,15 +2,18 @@ const Lang = {
     INVALID_REFRESH_TOKEN: "Invalid refresh token.",
     INVALID_ADDRESS: "Invalid address",
     USER_NOT_FOUND: "User not found",
-    NOT_ON_ALLOWED_LIST: "Wallet address not found on allowlist",
-    ADDRESS_BLOCKED: "Your wallet address is blocked",
-    NFT_NOT_FOUND: "You dont have the NFT to access site",
+    NOT_ON_ALLOWED_LIST: "Address of wallet not found on allowlist.",
+    ADDRESS_BLOCKED: "Your wallet address has been blocked.",
+    NFT_NOT_FOUND: "You cannot access the site without the NFT.",
     INVALID_SIGNATURE: "Invalid signature",
-    INCORRECT_WALLET_ADDRESS: "Your wallet address and signed message wallet address is not same.",
+    INCORRECT_WALLET_ADDRESS: "Your wallet address and the wallet address for the signed message are different.",
     EXPIRED_NONCE: "Expired sign-in. Try again",
     INCORRECT_CRON_PASSWORD: "Incorrect cron password",
-    COUNTRY_BLOCKLIST: "Not allowed in sign in from your country",
-    INVALID_API_SECRET: "Invalid API Key / Secret"
+    COUNTRY_BLOCKLIST: "Signing in from your country is prohibited.",
+    INVALID_API_SECRET: "Invalid API Key / Secret",
+    WALLET_LINKED_SUCCESSFULLY: "Your wallet were successfully connected.",
+    WALLET_ALREADY_LINKED: "You have already linked this wallet address to your account.",
+    WALLET_LINK_ERROR: "There is already an account linked to this wallet address.",
 }
 
 export default Lang;
