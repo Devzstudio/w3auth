65d24650ec6a36949d13babe06f97435a117d0f9
Pjijin <pjijin1@gmail.com>
2022-08-03 20:16:58 +0530

    🐛 Fix: ui improvements

--- components/settings/ChainCard.tsx
+++ components/settings/ChainCard.tsx
@@ -1,6 +1,13 @@
 import { Switch } from '@mantine/core';
 
-const ChainCard = ({ label, checked, onChange, icon = null }) => {
+interface IChainCard {
+	label: string;
+	checked: boolean;
+	onChange: (e) => void;
+	icon?: string;
+}
+
+const ChainCard = ({ label, checked, onChange, icon = null }: IChainCard) => {
 	return (
 		<div className="bg-white border dark:border-transparent dark:bg-dark-800 rounded p-5 col-span-3">
 			<div className="flex justify-between items-center">
